afc590b4dbe820627d9d5bd18ec4d71b594f8915
Bui Huy <huybui2451@gmail.com>
2023-08-04 11:34:38 +0700

    bai tap ngay 14

--- Day_14/js/script.js
+++ Day_14/js/script.js
@@ -7,7 +7,7 @@ Bài 1: Hoán vị 2 số
 var a = 8,
     b = 6;
 
-a = a + b; // 18
+a += b; // 18
 b = a - b; // 8
 a = a - b; // 10
 // b = (a + b) - a;
@@ -19,7 +19,6 @@ console.log(`Kết quả là: ${a} ${b}`);
 Bài 2: Thực hiện phép toán
  S = 10 + 20 + 5^10 / 2
 */
-
 var S = 10 + 20 + 5**10 / 2;
 
 console.log(S);
@@ -43,7 +42,6 @@ if (j >= k && j >= m) {
 } else if (m >= j && m >= k) {
     biggestNumber = m;
 }
-
 console.log(`Số lớn nhất là : ${biggestNumber}`)
 
 
@@ -78,29 +76,48 @@ Output:
 Thực hiện đổi chỗ 3 số a, b, c sao cho 3 số có thứ tự tăng dần
 */
 
-var z = 2,
-    x = 10,
-    c = 10,
-    one,
-    three;
+var z = 10, x = 18, c = 0,
+    biggest = z;
 
-var total = z + x + c;
-
-if (z <= x && z <= c) {
-    one = z;
-} else if (x <= z && x <= c) {
-    one = x;
-} else if (c <= x && c <= z) {
-    one = c;
+if (z > x) {
+    z = x // 18
+    x = biggest // 10
 }
-
-if (z >= x && z >= c) {
-    three = z;
-} else if (x >= z && x >= c) {
-    three = x;
-} else if (c >= x && c >= z) {
-    three = c;
+// 10, 18, 0
+if (z > c) {
+    z = c // 0
+    c = biggest // 10
 }
+// 0, 18, 10
+if (x > c) {
+    biggest = x // 18
+    x = c // 10
+    c = biggest // 18
+}
+// 0, 10, 18
 
-var two = total - one - three
-console.log(`Sắp xếp ${one} < ${two} < ${three}`);
+console.log(`Sắp xếp 3 số tăng dần ${z}, ${x}, ${c}`);
+
+
+// var z = 2, x = 0, c = -4,
+//     smallest, biggest;
+
+// if (z <= x && z <= c) {
+//     smallest = z;
+// } else if (x <= z && x <= c) {
+//     smallest = x;
+// } else if (c <= x && c <= z) {
+//     smallest = c;
+// }
+
+// if (z >= x && z >= c) {
+//     biggest = z;
+// } else if (x >= z && x >= c) {
+//     biggest = x;
+// } else if (c >= x && c >= z) {
+//     biggest = c;
+// }
+
+// var fit = (z + x + c) - smallest - biggest;
+
+// console.log(`Sắp xếp ${smallest}, ${fit}, ${biggest}`);
