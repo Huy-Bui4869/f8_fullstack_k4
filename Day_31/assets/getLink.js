dfd2f2fd7e56f572a77048d66cf1903e7d33de5e
Bui Huy <huybui2451@gmail.com>
2023-10-07 02:13:23 +0700

    bai tap ngay 31

--- Day_31/assets/getLink.js
+++ Day_31/assets/getLink.js
@@ -1,48 +1,46 @@
+"use strict";
+
 const counter = document.querySelector(".counter")
 const linkBtn = document.querySelector(".link-btn")
 
-// let count;
 let totalTime = 20;
-let stopId, start;
+let requestId, start;
 
 function checkTimes(currTime) {
-  // console.log(currTime);
   
   if (start === undefined) {
     start = currTime
   }
 
   //Tạo bộ đếm tính theo giây
-  // let curr = Math.floor((currTime - start) / 1000);
-  // let count = totalTime - curr;
-  let count = totalTime - parseInt((currTime - start) / 1000);
-  console.log(count);
+  let count = parseInt((currTime - start) / 1000);
 
-  counter.textContent = count;
+  counter.textContent = (totalTime - count);
 
-  if (count >= 0) {
-    stopId = requestAnimationFrame(checkTimes);
+  if ((totalTime - count) > 0) {
+    requestId = requestAnimationFrame(checkTimes);
   } else {
-    // count = 0
     linkBtn.disabled = false;
     linkBtn.style.cursor = "pointer"
-    linkBtn.addEventListener("click", function () {
-      // console.log("click me");
-      window.location.href="https://fullstack.edu.vn"
-    });
   }
 }
+
+// document.addEventListener("visibilitychange", (e) => {
+//   if (document.visibilityState === "visible") {
+//     requestAnimationFrame(checkTimes)
+//   } else {
+//     cancelAnimationFrame(requestId)
+//   }
+// });
+
+requestAnimationFrame(checkTimes)
 
 window.onload = function () {
   linkBtn.disabled = true;
   linkBtn.style.cursor = "no-drop"
-
 };
 
-document.addEventListener("visibilitychange", () => {
-  if (document.visibilityState === 'visible') {
-    window.requestAnimationFrame(checkTimes)
-  } else {
-    cancelAnimationFrame(stopId)
-  }
+linkBtn.addEventListener("click", function () {
+  // console.log("click me");
+  window.location.href="https://fullstack.edu.vn"
 });
